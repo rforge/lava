79b7bc9fa55dc9a6cb3e5126de6eaee0dd029a31
klaus <klaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-09 16:07:22 +0000

    Composite Likelihood with support for data missing at random
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lava.tobit/R/clprobit.R
+++ pkg/lava.tobit/R/clprobit.R
@@ -11,21 +11,25 @@ clprobit <- function(x,data,k=2,type=c("nearest","all"),pairlist,silent=TRUE,
     }
   } else {
     mypar <- pairlist
-  }
+  }  
   mydata <- c()
-  for (ii in 1:ncol(mypar)) {
+  nblocks <- ncol(mypar)
+  for (ii in 1:nblocks) {
     data0 <- data; data0[,binpos[-mypar[,ii]]] <- NA
     mydata <- rbind(mydata,data0)
-  }  
+  }
   e0 <- estimate(x,data=mydata,missing=TRUE,silent=silent,
               ...)
   S <- score(e0,indiv=TRUE)
-  Siid <- S[[1]]; Siid[is.na(Siid)] <- 0
-  for (i in 2:length(S)) {
-    S0 <- S[[i]]; S0[is.na(S0)] <- 0
-    Siid <- Siid+S0
+  nd <- nrow(data)
+  block1 <- which((1:nd)%in%(rownames(S)))
+  blocks <- sapply(1:nblocks, function(x) 1:length(block1)+length(block1)*(x-1))
+  Siid <- matrix(0,nrow=length(block1),ncol=ncol(S))
+  for (j in 1:ncol(blocks)) {
+    Siid <- Siid+S[blocks[,j],]
   }
   iI <- vcov(e0); J <- t(Siid)%*%(Siid)
+  e0$iidscore <- Siid
   e0$vcov <- iI%*%J%*%iI
   cc <- e0$coef; cc[,2] <- sqrt(diag(e0$vcov))
   cc[,3] <- cc[,1]/cc[,2]; cc[,4] <- 2*(1-pnorm(cc[,3]))
