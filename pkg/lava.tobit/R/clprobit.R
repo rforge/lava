5f1d662641a80890080a911740345376ddfccc32
klaus <klaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-28 18:33:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lava.tobit/R/clprobit.R
+++ pkg/lava.tobit/R/clprobit.R
@@ -1,23 +1,36 @@
 clprobit <- function(x,data,k=2,type=c("nearest","all"),pairlist,silent=TRUE,
                      ...) {
-  bin <- binary(x)
-  binpos <- which(colnames(data)%in%bin)
-  if (length(bin)<(k+1)) stop("No need for composite likelihood analysis.")
+  y <- endogenous(x)
+  binsurv <- rep(FALSE,length(y))
+  for (i in 1:length(y)) {
+    z <- data[,y[i]]
+    binsurv[i] <- is.Surv(z) | (is.factor(z) && length(levels(z))==2)
+  }
+  
+  binsurv <- unique(c(y[binsurv],binary(x)))
+  binsurvpos <- which(colnames(data)%in%binsurv)
+  if (length(binsurv)<(k+1)) stop("No need for composite likelihood analysis.")
   if (missing(pairlist)) {
     if (type[1]=="all") {
-      mypar <- combn(length(bin),k) ## all pairs (or multiplets), k=2: k*(k-1)/2
+      mypar <- combn(length(binsurv),k) ## all pairs (or multiplets), k=2: k*(k-1)/2
     } else {
-      mypar <- sapply(0:(length(bin)-k), function(x) x+1:k)
+      mypar <- sapply(0:(length(binsurv)-k), function(x) x+1:k)
     }
   } else {
     mypar <- pairlist
   }  
-  mydata <- c()
+
+  mydata <- data[1,,drop=FALSE][-1,]
   nblocks <- ncol(mypar)
   for (ii in 1:nblocks) {
-    data0 <- data; data0[,binpos[-mypar[,ii]]] <- NA
+    data0 <- data;
+    for (i in binsurvpos[-mypar[,ii]]) {
+      data0[,i] <- NA
+      if (is.factor(data[,i])) data0[,i] <- factor(data0[,i],levels=levels(data[,i]))
+    }    
     mydata <- rbind(mydata,data0)
   }
+
   suppressWarnings(e0 <- estimate(x,data=mydata,missing=TRUE,silent=silent,
               ...))
   S <- score(e0,indiv=TRUE)
@@ -33,7 +46,7 @@ clprobit <- function(x,data,k=2,type=c("nearest","all"),pairlist,silent=TRUE,
   e0$blocks <- blocks
   e0$vcov <- iI%*%J%*%iI ## thetahat-theta0 :=(asymp) I^-1*S => var(thetahat) = iI*var(S)*iI 
   cc <- e0$coef; cc[,2] <- sqrt(diag(e0$vcov))
-  cc[,3] <- cc[,1]/cc[,2]; cc[,4] <- 2*(1-pnorm(cc[,3]))
+  cc[,3] <- cc[,1]/cc[,2]; cc[,4] <- 2*(1-pnorm(abs(cc[,3])))
   e0$coef <- cc
   class(e0) <- c("clprobit",class(e0))
   return(e0)
