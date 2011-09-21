92547e8db1a814dc894daa182d59ba9d4d2ac139
klaus <klaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-21 13:20:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@129 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bptwin/R/ipw.R
+++ pkg/bptwin/R/ipw.R
@@ -1,4 +1,4 @@
-ipw <- function(formula,data,cluster,samecens=TRUE,obsonly=TRUE,weight="w",
+ipw <- function(formula,data,cluster,samecens=TRUE,obsonly=TRUE,weightname="w",
                 pairs=TRUE) {
   require(rms)
   fit <- cph(formula,data=data,surv=TRUE,x=TRUE,y=TRUE)
@@ -9,19 +9,21 @@ ipw <- function(formula,data,cluster,samecens=TRUE,obsonly=TRUE,weight="w",
   pr <- survest(fit,what="parallel",newdata=data,times=otimes-delta)
   if (samecens & !missing(cluster)) {
     noncens <- with(data,!eval(terms(formula)[[2]][[3]]))
-    data[,weight] <- pr
+    data[,weightname] <- pr
     data <- data[noncens,,drop=FALSE]
     data <- data[order(data[,cluster]),,drop=FALSE]
     id <-  table(data[,cluster])
     if (pairs) {
       gem <- data[,cluster]%in%(names(id)[id==2])
+      id <- id[id==2]
       data <- data[gem,]
     }
-    id <- data[,cluster]
-    for (i in unique(id)) {
-      data[id==i,weight] <- min(data[id==i,weight])
-    }
-    data[,weight] <- 1/data[,weight]
+    d0 <- subset(data,select=c(cluster,weightname))
+    timevar <- paste("_",cluster,weightname,sep="")
+    d0[,timevar] <- unlist(lapply(id,seq))
+    W <- apply(reshape(d0,direction="wide",timevar=timevar,idvar=cluster),1,
+               function(x) min(x,na.rm=TRUE))
+    data[,weightname] <- 1/rep(W,id)
     return(data)
   }
   return(pr)
