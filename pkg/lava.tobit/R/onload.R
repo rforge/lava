9af6905b5e1a1da71bc83e0a833502badf4f2d1d
klaus <klaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-13 07:15:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lava.tobit/R/onload.R
+++ pkg/lava.tobit/R/onload.R
@@ -1,4 +1,4 @@
-'.onLoad' <- function(lib, pkg="lava.tobit") {
+'.onAttach' <- function(lib, pkg="lava.tobit") {
   addhook("lava.tobit.estimate.hook","estimate.hooks")
   addhook("lava.tobit.color.hook","color.hooks")
   addhook("lava.tobit.sim.hook","sim.hooks")
@@ -7,6 +7,6 @@
   lava.options(tobitAlgorithm=mvtnorm::GenzBretz(abseps=1e-5),
                tobitseed=1, threshold=1)
   desc <- utils::packageDescription(pkg)
-  cat("Loading '", desc$Package, "' package...\n", sep="")
-  cat("Version    : ", desc$Version, "\n", sep="")
+  packageStartupMessage("\nLoading '", desc$Package, "' package...\n",
+                        "Version    : ", desc$Version, "\n")
 }
