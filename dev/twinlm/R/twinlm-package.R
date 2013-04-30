652e8de6ed7f4fb332011e2f88ed1ce439005f0e
klaus <klaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-30 07:42:35 +0000

    root dir cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@629 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- dev/twinlm/R/twinlm-package.R
+++
@@ -1,9 +0,0 @@
-##' Linear models for twin data
-##' 
-##' Implementation of the classical twin variance component models (ACE/ADE/DCE/DE/AE)##'
-##' 
-##' @name twinlm-package
-##' @docType package
-##' @author Klaus K. Holst
-##' @import stats utils lava
-NULL
