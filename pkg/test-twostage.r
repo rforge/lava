b990a4989b83b6a0ea3049bbe86a6fd637f82cc9
scheike <scheike@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-03 13:21:59 +0000

    simCO update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lava@1269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test-twostage.r
+++ pkg/test-twostage.r
@@ -1,5 +1,24 @@
 
 library(mets)
-example(twostage)
-example(easy.twostage)
+###example(twostage)
+###example(easy.twostage)
+###
+
+library(mets)
+x=1:2
+
+.Call("claytonoakesR",1,1,1,0.5,0.3)
+.Call("claytonoakesR",1,1,0,0.5,0.3)
+.Call("claytonoakesR",1,0,1,0.5,0.3)
+.Call("claytonoakesR",1,0,0,0.5,0.3)
+
+
+theta <- runif(5)+0.1
+s1 <- rbinom(5,1,0.5)
+s2 <- rbinom(5,1,0.5)
+p1 <- runif(5)
+p2 <- runif(5)
+
+.Call("claytonoakesR",theta,s1,s2,p1,p2)
+
 
